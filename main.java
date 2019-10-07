ee9436261c6ea030657b9f0f3bbc584984eb7386
dackyy <eorl4940@naver.com>
2019-10-07 13:04:28 +0900

    main 메소드 생성후 Scanner를 이용 한라인씩 읽어들이는 메소드 생성

---
+++ main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+/**
+ * 여기에 main 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class main
+{  
+    public static void main(String[] args){
+        try{
+            Scanner scanner = new Scanner(new FileReader("inputData20191007.txt"));
+
+            int c;
+            while(scanner.hasNext())
+            {
+                String word = scanner.nextLine();
+                wordVector.add(word);
+            }
+            scanner.close();
+        } catch(IOException e){
+            System.out.println("파일을 읽을 수 없습니다.");   
+        }
+    }
+}
